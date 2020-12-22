e5f09205966cc4dda12e279e9b903e6726bf3415
Kukuvz <antonslayer@gmail.com>
2020-12-22 09:59:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+from art import logo
+from art import vs
+from game_data import data
+
+SCORE = 0
+
+print(logo)
+print("Compare A: ")
+
+print(vs)
+print("Agaist B: ")
+
+choice = input("Who has more followers? Type 'A' or 'B': ")
