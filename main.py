66feb5063e0b622f02fe6cdde97c17e5e3eaa747
Kukuvz <antonslayer@gmail.com>
2020-12-22 20:30:23 +0000

    Final_version

--- main.py
+++ main.py
@@ -1,65 +1,67 @@
 import random
-from replit import clear
-from art import logo
-from art import vs
+from art import logo, vs
 from game_data import data
+from replit import clear
 
-SCORE = 0
+def format_data(account):
+  """Format account data to print"""
+  account_name = account["name"]
+  account_desc = account["description"]
+  account_country = account["country"]
+  return f"{account_name}, a {account_desc}, from {account_country}"
 
-def print_a_b(dict_a, dict_b):
-  name_a = dict_a["name"]
-  follow_a = dict_a["follower_count"]
-  description_a = dict_a["description"]
-  country_a = dict_a["country"]
-  print(f"Compare A: {name_a}, a {description_a}, from {country_a}")
-  print(f"Pssst, followers = {follow_a}")
-  name_b = dict_b["name"]
-  follow_b = dict_b["follower_count"]
-  description_b = dict_b["description"]
-  country_b = dict_b["country"]
+def check_answer(guess, a_followers, b_followers):
+  """Take the user guess and follower counts and returns if they got it right"""
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"  
+
+#Display art
+print(logo)
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+#Make game repeatable
+while game_should_continue:
+
+  #Generate random account
+
+  #Making account at B become next account position A
+  account_a = account_b
+  account_b = random.choice(data)
+
+  if account_a == account_b:
+    account_b = random.choice(data)
+
+  print(f"Compare A: {format_data(account_a)}")
   print(vs)
-  print(f"Against B: {name_b}, a {description_b}, from {country_b}")
-  print(f"Pssst, followers = {follow_b}")
+  print(f"Compare B: {format_data(account_b)}")
 
-def compare(member_a, member_b):
-  follow_a = member_a["follower_count"]
-  follow_b = member_b["follower_count"]
-  if follow_a > follow_b:
-    return member_a
-  elif follow_a < follow_b:
-    return member_b   
-def game():
+  #Ask user
+  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+
+  #Check if user is correct
+  ##Get follower count of each account
+  a_follower_count = account_a["follower_count"]
+  b_follower_count = account_b["follower_count"]
+  is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+  #clear screen
+  clear()
   print(logo)
-  member_a = random.choice(data)
-  should_continue = False
 
-  while should_continue == False:
-    member_b = random.choice(data)
-    print_a_b(member_a, member_b)
+  #Give user feedback
+  #Score keeping
+  if is_correct:
+    score += 1
+    print(f"You`re right! Current score: {score}")
+  else:
+    game_should_continue = False
+    print(f"Sorry, that`s wrong. Final score: {score}")  
 
-    choice = (input("Who has more followers? Type 'A' or 'B'")).lower()
-    ab_choice = {}
-    if choice == "a":
-      ab_choice = member_a
-    else:
-      ab_choice = member_b 
 
-    winner = compare(member_a, member_b)
-   
-    if ab_choice == winner:
-      global SCORE
-      SCORE+=1
-      member_a = winner
-      clear()
-      game()
-    elif ab_choice != winner:
-      should_continue == True
-      clear()
-      print(f"You Lose. Your score: {SCORE}")
-  # clear()
-  # print(logo)
-  # print(f"You Lose. Your score: {SCORE}")
-  
-        
 
-game()
+
